8844dc2b30b15a8eef2c4869d9e0c014e11896c6
Wolmin <lamonos123@gmail.com>
2024-09-23 14:53:05 +0200

    Kill worker for dockerized server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 cd ..
-source ./venv/bin/activate
-gunicorn --timeout 1800 --bind 0.0.0.0:8000 api_server:app
+
+(sleep 5 && kill 10)&
+gunicorn --bind 0.0.0.0:8000 api_server 
